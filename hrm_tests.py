3492419a5903ed4ec76b0ea9e3ec0d0129bf6150
Franck Pommereau <franck.pommereau@gmail.com>
2022-04-06 13:47:51 +0200

    fixed tikz export, added direct draw

--- hrm_tests.py
+++ hrm_tests.py
@@ -1,21 +1,32 @@
 # coding: utf-8
 
-import json, collections, operator, sys
+import json, collections, operator, sys, pathlib
 import tqdm
 
 from colorama import Fore as F, Style as S
 from IPython.core import ultratb
 
 from hrm import HRM
+from hrm.tikz import draw
 
 log = tqdm.tqdm(sorted(json.load(open("solutions/solutions.json")),
                        key=operator.itemgetter("levelNumber")))
+pdf_base = pathlib.Path("pdf")
 
 for sol in log :
+    path = f"solutions/{sol['path']}"
     try :
-        hrm = HRM.parse(f"solutions/{sol['path']}")
+        hrm = HRM.parse(path)
     except :
         log.write(f"{F.RED}parse error:{S.RESET_ALL} {sol['path']}")
+    # draw code
+    pdf = (pdf_base / sol["path"]).with_suffix(".pdf")
+    pdf.parent.mkdir(parents=True, exist_ok=True)
+    try :
+        draw(path, pdf)
+    except :
+        log.write(f"{F.YELLOW}draw failed:{S.RESET_ALL} {sol['path']}")
+    # exec code
     num = sol["levelNumber"]
     lvl = hrm.level(num)
     for ex, example in enumerate(lvl["examples"]) :
